822de5aef8c08084e11610553416009aab11f31f
Thomas Viana <thomasviana16@gmail.com>
2021-07-30 16:53:05 -0500

    More animations

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/helpers/custom_route.dart';
 import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/user_products_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/auth.dart';
+import 'package:shop_app/helpers/custom_route.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -27,8 +29,9 @@ class AppDrawer extends StatelessWidget {
             leading: Icon(Icons.payment),
             title: Text('Orders'),
             onTap: () {
-              Navigator.of(context)
-                  .pushReplacementNamed(OrdersScreen.routeName);
+              Navigator.of(context).pushReplacement(CustomRoute(
+                builder: (ctx) => OrdersScreen(),
+              ));
             },
           ),
           Divider(),
